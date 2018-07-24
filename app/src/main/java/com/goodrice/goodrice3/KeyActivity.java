e736a65140dfc3d67dee6ccc83ee050edd80c1a3
8236fn <40617124+8236fn@users.noreply.github.com>
2018-07-25 00:00:02 +0800

    20180724-menu
    增加menu功能

--- app/src/main/java/com/goodrice/goodrice3/KeyActivity.java
+++ app/src/main/java/com/goodrice/goodrice3/KeyActivity.java
@@ -1,6 +1,8 @@
 package com.goodrice.goodrice3;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -63,6 +65,43 @@ public class KeyActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.privacy:
+                AlertDialog.Builder builder = null;
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("使用條款")
+                        .setMessage("『呷好米』APP僅提供您快速查詢行政院農委會提供之市售包裝米之檢驗結果，不會收集您的任何個人資料，敬請安心使用")
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+            case R.id.copyright:
+                AlertDialog.Builder builder2 = null;
+                builder2 = new AlertDialog.Builder(this);
+                builder2.setTitle("版權宣告")
+                        .setMessage("『呷好米』APP為 陳彥伃、蔡鴻淇、黃湧崑 於 2018年健行科技大學推廣教育中心-Android程式設計班，共同創作完成之成果。")
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+            case R.id.disclaimer:
+                AlertDialog.Builder builder3 = null;
+                builder3 = new AlertDialog.Builder(this);
+                builder3.setTitle("免責聲明")
+                        .setMessage("『呷好米』APP所提供之資料皆自『行政院農業委員會資料開放平台』取得，市售包裝米之檢驗結果與品質皆與本APP無關。若對資料內容有任何疑義，請以農委會資料開放平台網站為準。")
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).show();
+                break;
+
+
+        }
         return super.onOptionsItemSelected(item);
     }
 }
